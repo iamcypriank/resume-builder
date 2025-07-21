df7f9cc3f28afcee3a77fe33fda833b1cefe7e1a
Cyprian Kujur <cypriankujur51@gmail.com>
2025-07-21 15:22:36 +0530

    initital setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="w-[400px] h-[400px] bg-green-300 p-4 text-white">
+      <h1 className="font-[200]">resume</h1>
+      <p>name</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+       Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
